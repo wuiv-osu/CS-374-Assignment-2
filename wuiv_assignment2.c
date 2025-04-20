2c4d4ec487be45fbf5685da744e99a4c2440e5f4
wuiv-osu <wuiv@oregonstate.edu>
2025-04-20 13:20:11 -0500

    Create wuiv_assignment2.c

---
+++ wuiv_assignment2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct movie {
+  char* title;
+  int year;
+  char* languages;
+  float rating;
+};
+
+struct movie* createMovie(char* line){
+    
+
+
+};
